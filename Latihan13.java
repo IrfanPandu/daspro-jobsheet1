7d36404a1d8911c879f07a767b3d2bfe33a80a6d
Irfan Pandu Pratama <154867525+IrfanPandu@users.noreply.github.com>
2024-09-06 08:24:21 +0700

    Add files via upload

---
+++ Latihan13.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan13
+ */
+
+ public class Latihan13{
+    public static void main(String[] args) {
+        System.out.print("halo nama saya irfan pandu pratama");
+        System.out.print("\nsaya adalah mahasiswa baru jurusan Teknologi Informasi--POLINEMA ");
+    }
+}
---
+++ tugas1.java
@@ -0,0 +1,19 @@
+/**
+ * tugas1
+ */
+public class tugas1{
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Irfan Pandu Pratama");
+        System.out.println("NIM     : 244107020193");
+        System.out.println("Kelas   : 1E");
+        System.out.println("No Absen: 13"); 
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Alamat  : GondangLegi Prambon Nganjuk");
+        System.out.println("No Hp   : 081232116906");
+        System.out.println("Hobi    : basket");
+    }
+}
